2ea962f09c7ef47df5aef81f44835e666e7f6516
Shivam Gupta <shivamguptakrm@gmail.com>
2022-08-10 02:40:26 +0530

    delete some comment

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
 
-// console.log(date());
+
 
 const app = express();
 
